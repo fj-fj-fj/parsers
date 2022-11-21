cc883bc9be9cea6723f148e275a1b224b0f1a33d
fj-fj-fj <x1x1xx0@protonmail.com>
2022-11-21 23:32:36 +0300

    refactor src/kwargs

--- src/kwargs.py
+++ src/kwargs.py
@@ -1,25 +1,34 @@
-
+"""The 'kwargs' module contains Configuration() for default **kwargs."""
 from dataclasses import dataclass
 
 
 @dataclass
 class Configuration:
-    """Contains keys: url, file_parsed, file_response, parser"""
+    """Default kwargs"""
+
     url: str = None
+    parser: str = None
     file_parsed: str = None
     file_response: str = None
-    parser: str = None
 
-    def __post_init__(self):
+    def __post_init__(self) -> None:
+        """Create container with Configuration() attribute names."""
         self.__KeyStorage = type('__KeyStorage', (), {})
         for key in self.__dict__:
             if key:
                 setattr(self.__KeyStorage, key, key)
 
-    def setdefault(self, key: str, value: str):
+    def setdefault(self, key: str, value: str) -> None:
+        """Set new value to attribute if it is None."""
         if getattr(self, key) is None:
             setattr(self, key, value)
 
     @property
     def key(self):
+        """Return container with Configuration() attribute names.
+
+        All attributes contain names of the same name:
+            `Literal['url', 'parser', 'file_parsed', 'file_response']`
+
+        """
         return self.__KeyStorage
