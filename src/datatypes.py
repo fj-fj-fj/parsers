cca3556521d4acc7bc375c3c4db7d58ffbd5edb5
fj-fj-fj <x1x1xx0@protonmail.com>
2022-11-21 21:15:57 +0300

    feat: datatypes

--- src/datatypes.py
+++ src/datatypes.py
@@ -1,6 +1,22 @@
-"""Module '_types' that contains request types."""
+"""Module 'datatypes' that contains data structures and types."""
+from typing import Literal
 from typing import TypeVar
 
-Proxy = str
-ProxyList = list[Proxy]
+# config
+NamespaceLiteral = Literal['cli', 'file', 'url', 'parse', 'magic_numbers', 'timeouts']
+
+# request
+ProxyType = str
+ProxyList = list[ProxyType]
 TimeoutType = TypeVar('TimeoutType', float, tuple[float, float], None)
+
+# saving data
+JsonStr = HTML = str
+Data = TypeVar('Data', bytes, HTML, JsonStr, list[str])
+
+# decorators
+class classproperty(property):
+    """Decorator @staticmethod & @property"""
+
+    def __get__(self, cls, owner) -> type:
+        return classmethod(self.fget).__get__(None, owner)()
