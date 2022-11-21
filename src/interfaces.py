1350854d9e36eafe6b2e0382f39301b2bc5fea6b
fj-fj-fj <x1x1xx0@protonmail.com>
2022-11-21 21:21:48 +0300

    feat: add interfaces

---
+++ src/interfaces.py
@@ -0,0 +1,29 @@
+"""Module 'interfaces'."""
+from datatypes import ProxyList
+
+
+class DataStorage:
+    """Interface for any storage saving data."""
+
+    def save(self, data: str | bytes | list) -> None:
+        raise NotImplementedError
+
+
+class AbstractProxy:
+    """Interface for proxy tools."""
+
+    def parse_proxies(self, *args, **kwargs) -> ProxyList:
+        raise NotImplementedError
+
+    def check(self, *args, **kwargs) -> bool:
+        raise NotImplementedError
+
+
+class AbstractRequest:
+    """Interface for request tools."""
+
+    def __init__(self) -> None:
+        self.proxy = AbstractProxy()
+
+    def get(self):
+        raise NotImplementedError
