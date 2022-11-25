ad88c7ea864cc59b16d01a2b7374f0192cd078e6
fj-fj-fj <x1x1xx0@protonmail.com>
2022-11-25 14:57:51 +0300

    fix create_parsers_template_structure and test

---
+++ src/user_parsers/roscomsvoboda/parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def main():
+    print("\033[0;32mHello from roscomsvoboda.main function!\033[0m")
+
+
+if __name__ == '__main__':
+    main()
