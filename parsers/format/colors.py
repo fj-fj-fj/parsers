fe8f1bc21ef907a5f6ea1f31b77b95026d639120
fj-fj-fj <x1x1xx0@protonmail.com>
2023-01-17 17:54:20 +0300

    create `Colors`, `remove_escape_sequences()`

--- parsers/format/colors.py
+++ parsers/format/colors.py
@@ -1,12 +1,32 @@
-from types import SimpleNamespace
-
-RED = '\033[31m'
-YELLOW = '\033[33m'
-NC = '\033[39'
-
-
-color = SimpleNamespace(
-    red=RED,
-    yellow=YELLOW,
-    nc=NC,
+__all__ = (
+    'Colors',
+    'remove_escape_sequences',
 )
+
+import re as _re
+from enum import StrEnum as _StrEnum
+
+
+class Colors(_StrEnum):
+    ESC = '\x1B['
+    BLUE = f'{ESC}0;36m'
+    RED = f'{ESC}31m'
+    YELLOW = f'{ESC}33m'
+    NC = f'{ESC}0m'
+
+
+def remove_escape_sequences(string) -> str:
+    """Remove the 7-bit C1 ANSI escape sequences from a `string`"""
+    ansi_escape = _re.compile(r'''
+        \x1B  # ESC
+        (?:   # 7-bit C1 Fe (except CSI)
+            [@-Z\\-_]
+        |     # or [ for CSI, followed by a control sequence
+            \[
+            [0-?]*  # Parameter bytes
+            [ -/]*  # Intermediate bytes
+            [@-~]   # Final byte
+        )
+    ''', _re.VERBOSE)
+    cleaned = ansi_escape.sub('', string)
+    return cleaned
