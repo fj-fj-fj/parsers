30be8b1f3e6fe377884118c3e82cd0529c2ace9d
fj-fj-fj <x1x1xx0@protonmail.com>
2023-02-05 22:30:17 +0300

    refactor: tab-separeted-constants, improve docstrig

--- parsers/user_parsers/youtube/constants.py
+++ parsers/user_parsers/youtube/constants.py
@@ -1,25 +1,35 @@
-"""
-Usage:
+# flake8: noqa E262
+# pyright: reportUnboundVariable=false
+"""This module is a set of constants and a settings file.
 
-    For download playlists:
-        1. PLAYLIST_ID: str = <DESIRED PLAYLIST ID>
-        2. PARAMS = PARAMS_PLAYLIST_ID
-        * Downloaded will be into './data/youtube/<DESIRED PLAYLIST ID>'
+Usage:
+    For download playlist:
+        1. _playlist_id = <PLAYLIST-ID>
+        1. uncomment `PLAYLIST_ID`
+        2. uncomment `PARAMS = PARAMS_PLAYLIST_ID`
 
     For download one video/audio:
-        1. VIDEO_ID: str = <DESIRED VIDEO ID>
-        2. PARAMS = PARAMS_VIDEO_ID
-        * Downloaded will be into './data/youtube/<DESIRED VIDEO ID>'
+        For download '/shorts/':
+            1. _shorts_id = <SHORTS-ID>
+            2. uncomment `SHORTS_ID`
+            3. uncomment `PARAMS = PARAMS_SHORTS_ID`
+        For download '/watch?v=':
+            1. _video_id = <VIDEO-ID>
+            2. uncomment `VIDEO_ID`
+            3. uncomment `PARAMS = PARAMS_WATCH_ID`
 
-    For downloading into './data/youtube/'
-        1. URL: str = <FULL URL>
+* Downloaded will be in './data/youtube/'
+
+Tab-separeted-constants will help you quickly find the settings you need.
 
 """
+__all__ = ['constant_locals']
+import typing as _t
 from types import SimpleNamespace as _namespace
 
 from parsers.constants import Constant as _base
 
-_URL = URL = 'https://youtube.com'
+URL: _t.Final = 'https://youtube.com'
 
 # Directory for storing final data
 PARSED_DIR = F'{_base.DIR.PARSED_DATA}youtube'
@@ -32,64 +42,43 @@ MOVE_SCRIPT = F'{_base.DIR.USER_PARSERS}youtube/move.sh'
 # Display result to the screen
 PRINT_TO_STDOUT = True
 
-# Checked by `curl`=======================================
-# The block of code below has been automatically generated
-# Delete condition or reasign `URL`
-# if 301 not in range(200, 400):
-#     URL = _base.URL.HTTPBIN_ORG
-# ========================================================
+
+if True:  # Read-only
+    PARAMS_PLAYLIST, PLAYLIST_ID = '/playlist?list=', ''
+    PARAMS_SHORTS, SHORTS_ID = '/shorts/', ''
+    PARAMS_WATCH, VIDEO_ID = '/watch?v=', ''
+    YOUTUBE_URL, PARAMS = URL, ''
 
 
-# # -------------------- with pytube.Playlist
-#
-# ----------------------------- playlist?list
-PARAMS_PLAYLIST, PLAYLIST_ID = '/playlist?list=', ''
-#
-_test = 'PL6plRXMq5RADf1Jore6YlgX9tIKau9PIY'
-# who(1st/2nd name)_about_nvidoes
-AK_PYTHON_12 = 'PLlb7e2G7aSpQhNphPSpcO4daaRPeVstku'
-DK_LINUX_12 = 'PLHHm04DXWzeKZycf_ZuBgxWdVBnrjE_mj'
-DK_BASICS_12 = 'PLHHm04DXWzeKuhdGFHSEWDpdgoYpjPptR'
-AS_NETWORKS_56 = 'PLtPJ9lKvJ4oiNMvYbOzCmWy6cRzYAh9B1'
-# PLAYLIST_ID = AK_PYTHON_12
-# PLAYLIST_ID = DK_LINUX_12
-# PLAYLIST_ID = DK_BASICS_12
-# PLAYLIST_ID = AS_NETWORKS_56
-# ...
-PLAYLIST_ID = _test
-PARAMS_PLAYLIST_ID = PARAMS_PLAYLIST + PLAYLIST_ID
+if 'pytube.Playlist':  # than uncomment PLAYLIST_ID
+    test = 'PL6plRXMq5RADf1Jore6YlgX9tIKau9PIY'
+    AK_PYTHON_12 = 'PLlb7e2G7aSpQhNphPSpcO4daaRPeVstku'
+    DK_LINUX_12 = 'PLHHm04DXWzeKZycf_ZuBgxWdVBnrjE_mj'
+    DK_BASICS_12 = 'PLHHm04DXWzeKuhdGFHSEWDpdgoYpjPptR'
+    AS_NETWORKS_56 = 'PLtPJ9lKvJ4oiNMvYbOzCmWy6cRzYAh9B1'
+    _playlist_id = 'PLKacnqDKwqBQeYSZldTBrRHWYNJxSiJJ6'
+    # PLAYLIST_ID: _t.Final = _playlist_id  #                      <<--  check
+    PARAMS_PLAYLIST_ID: _t.Final = PARAMS_PLAYLIST + PLAYLIST_ID
 
 
-# # --------------------- with pytube.YouTube
-#
-# ------------------------------------ shorts
-PARAMS_SHORTS, SHORTS_ID = '/shorts/', ''
-#
-ITS_LIKE_THAT = 'v742rQOimgY'
-# ...
-SHORTS_ID = ITS_LIKE_THAT
-PARAMS_SHORTS_ID = PARAMS_SHORTS + SHORTS_ID
-#
-# ----------------------------------- watch?v
-PARAMS_WATCH, VIDEO_ID = '/watch?v=', ''
-#
-_test = 'RcenQ6CR_zM'
-MUSIC_DJENT_10H = 'nBVwveVXkWs'
-# VIDEO_ID = MUSIC_DJENT_10H
-# ...
-VIDEO_ID = _test
-PARAMS_WATCH_ID = PARAMS_WATCH + VIDEO_ID
+if 'pytube.YouTube':
+    ## if /shorts/ than uncomment SHORTS_ID
+    ITS_LIKE_THAT = 'v742rQOimgY'
+    _shorts_id = ITS_LIKE_THAT
+    # SHORTS_ID: _t.Final = _shorts_id  #                         <<---  check
+    PARAMS_SHORTS_ID: _t.Final = PARAMS_SHORTS + SHORTS_ID
+
+    ## if /watch?v= than uncomment VIDEO_ID
+    MUSIC_DJENT_10H = 'nBVwveVXkWs'
+    _video_id = '36HrYaGT6Cw'
+    # VIDEO_ID: _t.Final = _video_id  #                           <<---  check
+    PARAMS_WATCH_ID: _t.Final = PARAMS_WATCH + VIDEO_ID
 
 
-# # --------------------------- BUILD THE URL
-#
-YOUTUBE_URL, PARAMS = URL, ''
-# PARAMS = PARAMS_PLAYLIST_ID
-# PARAMS = PARAMS_SHORTS_ID
-PARAMS = PARAMS_WATCH_ID
-# ...
-YOUTUBE_URL += PARAMS
-# -------------------------------------------
+## Make suitable PARAMS available
+# PARAMS = PARAMS_PLAYLIST_ID  # uncomment if playlist            <<---  check
+# PARAMS = PARAMS_SHORTS_ID  # uncomment if shorts                <<---  check
+# PARAMS = PARAMS_WATCH_ID   # uncomment if watch                 <<---  check
 
 if not PARAMS:
     from parsers.exceptions import URLError
@@ -98,23 +87,28 @@ if not PARAMS:
     raise URLError(f'Parameters are required to download video(s)\n{goto}\n')
 
 
-# --------------------------- COLLECT EXPORTS
+YOUTUBE_URL += PARAMS  # Final
+
 constant_locals = _namespace(
     # parsers.constants.Constant
     base=_base,
-
+    # URL
     URL=YOUTUBE_URL,
-    PLAYLIST_ID=PLAYLIST_ID,
-    VIDEO_ID=VIDEO_ID,
-    SHORTS_ID=SHORTS_ID,
-    URL_WITHOUT_PARAMS=_URL,
     PARAMS=PARAMS,
+    URL_WITHOUT_PARAMS=URL,
     # Flags
     PRINT_TO_STDOUT=PRINT_TO_STDOUT,
-    # TODO: ? PYTUBE_OBJ = Playlist | YouTube
     # Files
     PARSED_DIR=PARSED_DIR,
     SAMPLE_FILE=SAMPLE_FILE,
     PLAYLISTS=PLAYLISTS,
     MOVE_SCRIPT=MOVE_SCRIPT,
+
+    # Checking
+    PLAYLIST_ID=PLAYLIST_ID,
+    SHORTS_ID=SHORTS_ID,
+    VIDEO_ID=VIDEO_ID,
+    PARAMS_PLAYLIST=PARAMS_PLAYLIST,
+    PARAMS_SHORTS=PARAMS_SHORTS,
+    PARAMS_WATCH=PARAMS_WATCH,
 )
